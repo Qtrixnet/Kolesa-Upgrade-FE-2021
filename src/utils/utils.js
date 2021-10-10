044f5fd0e1a73721b1678793e95bba906d7ffa8d
Kirill Shashichev <67040346+Qtrixnet@users.noreply.github.com>
2021-10-10 23:20:54 +0600

    добавить закрытие по оверлею

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -4,24 +4,23 @@ import generateCard from './generators/generateCard';
 
 const deletePopup = () => (document.querySelector('.popup') ? document.querySelector('.popup').remove() : '');
 
-const openPopup = (evt) => {
-    const targetCard = evt.target.closest('.card');
+export const openPopup = (event) => {
+    const targetCard = event.target.closest('.card');
 
     page.insertAdjacentHTML('beforeEnd', generatePopup(targetCard));
 
     document.querySelector('.popup').classList.add('popup_opened');
     document.querySelector('.popup__close-button').addEventListener('click', deletePopup);
-    document.addEventListener('keyup', event => (event.key === 'Esc' || event.key === 'Escape' ? deletePopup() : ''));
+    document.addEventListener('keyup', evt => (evt.key === 'Esc' || evt.key === 'Escape' ? deletePopup() : ''));
+    document.addEventListener('click', evt => (evt.target.classList.contains('popup__overlay') ? deletePopup() : ''));
 };
 
 export const sortElements = arr => arr.sort(item => (item.isNew === true ? -1 : 1));
 
 export const renderCards = (cards, container) => {
-    document.querySelectorAll('.card').forEach(card => card.remove());
+    container.querySelectorAll('.card').forEach(card => card.remove());
 
     cards.forEach(card => container.insertAdjacentHTML('beforeEnd', generateCard(card)));
 
-    document.querySelectorAll('.card').forEach((card) => {
-        card.addEventListener('click', openPopup);
-    });
+    container.querySelectorAll('.card').forEach(card => card.addEventListener('click', openPopup));
 };
